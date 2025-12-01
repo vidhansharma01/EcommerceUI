e37106399a11a7f351a56a216aa6ee2d2a8c6cb1
Vidhan <vidhansharma01@gmail.com>
2025-12-01 18:46:37 +0530

    Event, Property & 2 way binding in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('My First Angular app');
+  title: string = 'My First Angular app';
+  imageUrl: string = 'https://picsum.photos/seed/picsum/200/300';
+  count: number = 0;
+  username: string = '';
+
+  increment(){
+    this.count++;
+  }
+
+  decrement(){
+    this.count--;
+  }
 }
