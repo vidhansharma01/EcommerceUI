ef1e3cc8b20f70cb5cc117db5dcf79f3e0d13e80
Vidhan <vidhansharma01@gmail.com>
2025-12-01 21:58:54 +0530

    Structural & Attributes Directives in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   title: string = 'My First Angular app';
   imageUrl: string = 'https://picsum.photos/seed/picsum/200/300';
   count: number = 0;
   username: string = '';
+  isLoggedIn: boolean = true;
 
-  increment(){
+  active: string = 'active';
+
+  increment() {
     this.count++;
   }
 
-  decrement(){
+  decrement() {
     this.count--;
   }
 }
