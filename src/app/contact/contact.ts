adddce6b465ecb8c0dc09fe5700e407bdc866f5f
Vidhan <vidhansharma01@gmail.com>
2026-01-03 17:33:52 +0530

    RxJs pipe & map operators

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostService } from '../services/post.service';
+import { Post } from '../services/post.interface';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [CommonModule],
+  providers: [PostService],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
   price: number = 10.5;
   user: any = { name: 'Vidhan', age: 25, skills: ['Angular', 'TypeScript', 'JavaScript'] };
+
+  post$: Observable<Post[]>;
+
+  constructor(private postService: PostService) {
+    this.post$ = this.postService.getPosts();
+  }
 }
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from './post.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+
+  getPostById(id: number): Observable<Post> {
+    return this.http.get<Post>(`${this.apiUrl}/${id}`);
+  }
+}
